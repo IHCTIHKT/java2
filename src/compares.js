f239ef5492c4757a688ecb62c3e312e221ecde94
Gaziav <bebraziana@gmail.com>
2025-09-12 20:49:14 +0300

    comparestrings

--- src/compares.js
+++ src/compares.js
@@ -1,7 +1,9 @@
-export function compare(a, b) {
-  return a === b;
-}
-console.log(compare(1, 2));
-console.log(compare(2, 3));
-console.log(compare(3, 4));
-console.log(compare(5, 5));
+export const compareStrings = (a, b) => a.toLowerCase() === b.toLowerCase();
+let text = 'Начало';
+let sub = `всех${text.slice(0, text.length - 1)}`;
+
+console.log(compareStrings(text, 'Начало'));
+
+text += sub;
+console.log(compareStrings(text, 'Начало всех'));
+console.log(compareStrings(text, 'начало всех начал'));
--- src/numbers.js
+++ src/numbers.js
@@ -1,9 +1,1 @@
-export function weather() {
-  const planet = Math.floor(Math.random() * 61 + 20) * 10;
-  const temperature = Math.floor(Math.random() * 1000000)
-    .toString()
-    .padStart(6, '0');
-  return `${planet}, ${temperature}`;
-}
-const temperature = weather();
-console.log(`Прогноз погоды на TerraRandomis: Температура завтра ${temperature} `);
+function
