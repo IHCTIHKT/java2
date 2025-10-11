470950a56de11bc79a15a188a379df006bd2dc9e
Gaziav <bebraziana@gmail.com>
2025-10-11 16:55:18 +0300

    destructuring - simple merge

--- src/arrays.js
+++ src/arrays.js
@@ -1,6 +1,13 @@
-nick: 'nickname', // не пустая строка перезапишет n: 1 на n: 2
-n: 9, // age было ложью и n: 2 переписался на n: 3, size = true и n: 3 перезаписан на n: 9
-size: 1, // выше сказал что size = true поэтому size 1
-x: 5 // known = false значит x: , далее photo = false x: 4 перезаписан на x: 5
+function mergeToNewObject(obj1, obj2) {
+  return {
+    ...obj1,
+    ...obj2,
+  };
+}
 
+const user = { name: 'Коля', age: 9 };
+const diplom = { degree: 'Магистр', university: 'МГУ', department: 'Кафедра информационных технологий' };
 
+const obj3 = mergeToNewObject(user, diplom);
+
+console.log(obj3);
