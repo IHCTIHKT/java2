2010b0d33f7ca4721248d3b8d53a0f7bfd8d967a
Gaziav <bebraziana@gmail.com>
2025-10-13 16:56:39 +0300

    destructuring - showInfo

--- src/arrays.js
+++ src/arrays.js
@@ -1,11 +1,38 @@
-function championShip(athletes) {
-  const [first, second, third, ...others] = athletes;
+function showInfo(user) {
+  const {
+    name,
+    surname,
+    patronymic = 'Не указано',
+    age,
+    gender = 'Не указано',
+    photo = 'https://photos.com/default.jpg',
+  } = user;
 
-  console.log(`1. Первое место: ${first}`);
-  console.log(`2. Второе место: ${second}`);
-  console.log(`3. Третье место: ${third}`);
-  console.log(`4. Остальные спортсмены: ${others}`);
+  const adult = age >= 18 ? 'Да' : 'Нет';
+
+  console.log('Карточка пользователя');
+  console.log(`Имя: ${name}`);
+  console.log(`Фамилия: ${surname}`);
+  console.log(`Отчество: ${patronymic}`);
+  console.log(`Возраст: ${age}`);
+  console.log(`Совершеннолетний: ${adult}`);
+  console.log(`Пол: ${gender}`);
+  console.log(`Фото: ${photo}`);
 }
+const ryan = {
+  name: 'Райан',
+  age: 28,
+  surname: 'Гослинг',
+  gender: 'Мужчина',
+  photo: 'https://photos.com/photo.jpg',
+};
 
-const athletes = ['Александр', 'Михаил', 'Валентин', 'Сергей', 'Артур'];
-championShip(athletes);
+const jason = {
+  name: 'Джейсон',
+  age: 5,
+  surname: 'Стетхем',
+  patronymic: 'Александрович',
+};
+
+showInfo(ryan);
+showInfo(jason);
