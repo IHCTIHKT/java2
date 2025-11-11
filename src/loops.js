7516f905a4ae7015525a431ee63f7bac4ddd9b69
Gaziav <bebraziana@gmail.com>
2025-11-11 18:10:06 +0300

    Скопированные задание сравнение массивов/обьектов, вместе.

--- src/loops.js
+++ src/loops.js
@@ -1,4 +1,20 @@
-export function compareArrays(arr1, arr2) {
+function compareObjects(obj1, obj2) {
+  const key1 = Object.keys(obj1);
+  const key2 = Object.keys(obj2);
+  if (key1.length !== key2.length) {
+    return false;
+  }
+  for (let key of key1) {
+    if (obj1[key] != obj2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(compareObjects({}, {})); // true
+console.log(compareObjects({ name: 'max' }, { name: 'john' }));
+
+function compareArrays(arr1, arr2) {
   if (arr1.length != arr2.length) {
     return false;
   }
