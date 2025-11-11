95e8ed2c454c6e263a5e15aef9bc74ff3470a4ee
Gaziav <bebraziana@gmail.com>
2025-11-11 18:10:36 +0300

    сравнение обьектов

--- src/loops.js
+++ src/loops.js
@@ -13,23 +13,3 @@ function compareObjects(obj1, obj2) {
 }
 console.log(compareObjects({}, {})); // true
 console.log(compareObjects({ name: 'max' }, { name: 'john' }));
-
-function compareArrays(arr1, arr2) {
-  if (arr1.length != arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] != arr2[i]) {
-      return false;
-    }
-  }
-  if (arr1.length == arr2.length) {
-    return true;
-  }
-}
-
-// ПРАВИЛЬНЫЕ ВЫЗОВЫ:
-console.log(compareArrays([], []));
-console.log(compareArrays([true, null, 1, 'stop'], [true, null, 1, 'stop']));
-console.log(compareArrays([1, 2, 3], [2, 1, 3]));
-console.log(compareArrays([null], []));
